3b073d1e0174ac374a58f9b4129e996175d22825
Robert Machi <robert.mc@hotmail.es>
2019-03-21 12:27:33 +0100

    The color of show Balance it change depending in the rang of value it has

--- js/app.js
+++ js/app.js
@@ -37,7 +37,20 @@ class UI {
   showBalance(){
     const expense = this.totalExpense();
     const total = parseInt(this.budgetAmount.textContent) - expense;
-    this.balanceAmount.textContent = total
+    this.balanceAmount.textContent = total;
+    if(total < 0){
+      this.balance.classList.remove('showGreen', 'showBlack');
+      this.balance.classList.add('showRed');
+    }
+    else if(total > 0){
+      this.balance.classList.remove('showRed', 'showBlack');
+      this.balance.classList.add('showGreen');
+    }
+    else if(total === 0){
+      this.balance.classList.remove('showGreen', 'showRed');
+      this.balance.classList.add('showBlack');
+    }
+    
   }
   // total expense
   totalExpense(){
